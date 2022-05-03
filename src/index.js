5c1425b9aa2a7167a0cf0ed8fc257e27300c83e8
myroslav <myrkozar@gmail.com>
2022-05-03 15:38:28 +0300

    ы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://pokeapi.co/api/v2/move-learn-method/2').then(response => {
+return response.json()}).then(pokemon => {
+    console.log(pokemon);
+}).catch(error => {
+    console.log(error);
+})
+
+
+
+
